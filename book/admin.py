a8f2f8b35dacc058403c600e46c39cb5045f4095
ryotaohashi-codor <ohashi@codor.co.jp>
2025-01-25 12:18:02 +0900

    a

---
+++ book/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Book, Review
+
+admin.site.register(Book)
+admin.site.register(Review)
+
+# from .models import SampleModel
+
+# admin.site.register(SampleModel)
+
+# Register your models here.
